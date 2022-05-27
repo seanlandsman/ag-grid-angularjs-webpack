06ce40ce926d6581ac53906222eda89b1ff6f825
seanlandsman <sean@thelandsmans.com>
2022-05-27 14:52:10 +0100

    initial work

---
+++ src/app/app.js
@@ -0,0 +1,48 @@
+import angular from 'angular';
+
+import {initialiseAgGridWithAngular1, ModuleRegistry, ClientSideRowModelModule} from "ag-grid-enterprise/dist/ag-grid-enterprise.esm.js";
+
+import '../style/app.css';
+
+let app = () => {
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
+};
+
+initialiseAgGridWithAngular1(angular);
+ModuleRegistry.registerModules([ClientSideRowModelModule])
+
+class AppCtrl {
+    constructor($scope) {
+        const columnDefs = [
+            {headerName: "Make", field: "make"},
+            {headerName: "Model", field: "model"},
+            {headerName: "Price", field: "price"}
+        ];
+
+        const rowData = [
+            {make: "Toyota", model: "Celica", price: "35000"},
+            {make: "Ford", model: "Mondeo", price: "32000"},
+            {make: "Porsche", model: "Boxter", price: "72000"}
+        ];
+
+        $scope.gridOptions = {
+            columnDefs: columnDefs,
+            rowData: rowData,
+            onGridReady: function (params) {
+                params.api.sizeColumnsToFit();
+            }
+        }
+    }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, ['agGrid'])
+    .directive('app', app)
+    .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
